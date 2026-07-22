0eee1da774c48dfd1ec87157e1c782ecce9430c5
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-07-22 23:10:19 +0700

    Create docker file

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,5 +1,5 @@
 // file này chứa các địa chỉ api chứ nó không gọi api
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
 const API = {
   // ───────── Client ─────────
